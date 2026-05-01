f37df05508dc37d739c536d02286c4bc3aae979e
Catsuri33 <louis.michault@protonmail.com>
2026-05-01 14:12:10 +0200

    Fix hyprland start

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -67,7 +67,7 @@
     waylandCompositors.hyprland = {
       prettyName = "Hyprland";
       comment = "Hyprland compositor managed by UWSM";
-      binPath = "/run/current-system/sw/bin/Hyprland";
+      binPath = "/run/current-system/sw/bin/start-hyprland";
     };
   };
 
